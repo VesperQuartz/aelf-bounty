ebe075013c896c4764f6b9b6df1d4f5464011c90
mrlectus <lilbrown3000@gmail.com>
2024-07-29 09:25:07 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  experimental: {
+    serverSourceMaps: false
+  }
+};
 
 export default nextConfig;
